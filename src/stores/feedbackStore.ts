fe9aedc632a8765b581d30135b57103bfe087e1d
hungpn37 <67941130+hungpn37@users.noreply.github.com>
2024-04-01 15:52:56 +0700

    fix parameter

--- src/stores/feedbackStore.ts
+++ src/stores/feedbackStore.ts
@@ -5,8 +5,8 @@ export const useFeedbackStore = defineStore('counter', () => {
   const rating = ref(3);
   const comment = ref('');
 
-  async function submitFeedback(query) {
-    const response = await fetch('https://en0ku7xcqv8yr.x.pipedream.net', {
+  async function submitFeedback(query: object) {
+    const response = await fetch('https://enoxohxi8ubq.x.pipedream.net', {
       method: 'POST',
       body: JSON.stringify({ rating: rating.value, comment: comment.value, query})
     });
